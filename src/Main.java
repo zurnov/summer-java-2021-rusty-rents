5b37ce558bd669e8f36831aaa179ed160ad174e9
Godfather22 <hristo2766@gmail.com>
2021-08-31 23:27:44 +0300

    Add Stuff

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,12 @@ import AppartmentActions.SellAppartment;
 import Database.Database;
 import LogIn.LogIn;
 import MainMenu.LandLordMainMenu;
+import Register.*;
 
 public class Main {
 
     public static void main(String[] args) {
         Database.establishConnection();
-      new LogIn();
+        new LogIn();
     }
 }
--- src/PLACEHOLDER/Main.java
+++
@@ -1,9 +0,0 @@
-package PLACEHOLDER;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Database.establishConnection();
-        LogIn test = new LogIn();
-    }
-}
