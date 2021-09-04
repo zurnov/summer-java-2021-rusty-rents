0c6fe34d0c810285f5544e37c5d676cf8c13a7a0
Bdaskalov <bdaskalov02@gmail.com>
2021-09-04 19:22:06 +0300

    Listing details GUI finished.

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,14 @@ import LogIn.*;
 import Account.*;
 import Options.*;
 import Register.*;
+import MainMenu.*;
 
 
 public class Main {
 
     public static void main(String[] args) {
         Database.establishConnection();
-        //new LogIn();
        // new ViewListings();
-        new ListingDetails();
+       new ListingDetails();
     }
 }
