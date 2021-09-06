1c7823df0bb8fc8a23137cd7692fa04bef6c097f
Godfather22 <hristo2766@gmail.com>
2021-09-06 14:16:27 +0300

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,10 @@
 import Database.*;
-import Listings.*;
 import LogIn.*;
-import Account.*;
-import Options.*;
-import Register.*;
-import MainMenu.*;
-
-import javax.swing.*;
-import java.awt.*;
-
 
 public class Main {
 
     public static void main(String[] args) {
         Database.establishConnection();
         new LogIn();
-        //new Register();
-        //new MyProfile();
-        //new MyListings();
-        new ViewListings();
-        //new ListingDetails(null);
-        //new ChangeEmail();
-        //new ChangePassword();
-        //new Options();
-
     }
 }
